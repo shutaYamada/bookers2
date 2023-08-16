95429e4ab26d07ee9db8b6535e1201dfd2333859
EC2 Default User <ec2-user@ip-172-31-29-177.ap-northeast-1.compute.internal>
2023-08-16 05:18:42 +0000

    users completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
+    def edit 
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        @books = @user.books
+    end
+    
+    def update
+    end
 end
