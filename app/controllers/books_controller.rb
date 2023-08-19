c903674268d2eaae7db67e1cbecc73e598695192
EC2 Default User <ec2-user@ip-172-31-29-177.ap-northeast-1.compute.internal>
2023-08-19 06:27:48 +0000

    all finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
         @book = Book.new
+        
     end
     
     def create
@@ -20,7 +21,7 @@ class BooksController < ApplicationController
     
     def show 
         @book = Book.find(params[:id])
-        
+        @book_comment = BookComment.new
     end
     
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about', as: 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  resources :books
   resources :users
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
   
 end
